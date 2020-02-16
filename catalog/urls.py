5628fab67e8d8bfbfd59231996534831b91a394d
Eduardo Pinheiro <eduardopinheiro20@outlook.com>
2020-02-16 13:52:32 -0300

    First commit in romo catalog

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path(r'^$', views.product_list, name='product_list')
+    path(r'^$', views.product_list, name='product_list'),
+    path(r'([\w_-]+)/$', views.category, name='category'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -8,3 +8,10 @@ def product_list(request):
     }
     return render(request, 'catalog/product_list.html', context)
 
+
+def category(request):
+    context = {
+        'current_category': None,
+        'product_list' : None
+    }
+    return render(request, 'catalog/category.html', context)
